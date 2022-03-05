8df0ecacdb8241a95396a6cead9e2ddf27bb5afb
shivam <shivamweb2@gmail.com>
2022-03-06 00:19:42 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>home page</h1>
+        <ul>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Home
